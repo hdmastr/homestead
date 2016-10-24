24cef6e0053d91f352a21fcfbec21c1ba2f7de16
Austen Cameron <austen.cameron@gmail.com>
2016-10-24 17:26:22 -0600

    added 'sudo' to after.sh commands

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -5,9 +5,9 @@
 # be run after the Homestead machine is provisioned.
 
 # Turn of MySQL 'strict' mode
-sed -i '/\[mysqld\]/a sql_mode=IGNORE_SPACE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION/' /etc/mysql/my.cnf
-service mysql restart
+sudo sed -i '/\[mysqld\]/a sql_mode=IGNORE_SPACE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION/' /etc/mysql/my.cnf
+sudo service mysql restart
 
 # Install Adminer (site defined in Homestead.yaml default)
-mkdir /usr/share/adminer
-wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/index.php
+sudo mkdir /usr/share/adminer
+sudo wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/index.php
