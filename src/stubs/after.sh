b37c19193bd0c8ad5ea188f6521e922acfd768f0
Austen Cameron <austen.cameron@gmail.com>
2016-10-24 20:01:08 -0600

    fixed after.sh stub to properly turn off strict mode and install adminer

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -1,13 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 
-# Turn of MySQL 'strict' mode
-sudo sed -i '/\[mysqld\]/a sql_mode=IGNORE_SPACE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION/' /etc/mysql/my.cnf
-sudo service mysql restart
+if [ ! -f /usr/local/homestead_extras_installed ]; then
+	echo "Installing Headmaster VM extras..."
 
-# Install Adminer (site defined in Homestead.yaml default)
-sudo mkdir /usr/share/adminer
-sudo wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/index.php
+	# Turn off MySQL 'strict' mode
+	echo "Turning off MySQL strict mode..."
+	sudo touch /etc/mysql/conf.d/disable_strict_mode.cnf
+	echo "[mysqld]" | sudo tee -a /etc/mysql/conf.d/disable_strict_mode.cnf > /dev/null 2>&1
+	echo "sql_mode=IGNORE_SPACE,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION" | sudo tee -a /etc/mysql/conf.d/disable_strict_mode.cnf > /dev/null 2>&1
+	sudo service mysql restart
+
+	# Install Adminer (site defined in Homestead.yaml default)
+	echo "Installing Adminer..."
+	sudo mkdir /usr/share/adminer
+	
+	# Remeber this the next time we try to run this
+	echo "Headmaster VM extras have been installed."
+	sudo touch /usr/local/homestead_extras_installed
+else
+	echo "Headmaster VM extras already installed... moving on..." 
+fi
