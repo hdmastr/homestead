337c5ec761ad3545403ae6e5588eb8aee643b3c7
Austen Cameron <austen.cameron@gmail.com>
2017-05-22 14:51:18 -0600

    fixed adminer install

--- resources/after.sh
+++ resources/after.sh
@@ -7,17 +7,10 @@
 if [ ! -f /usr/local/homestead_extras_installed ]; then
 	echo "Installing Headmaster VM extras..."
 
-	# Turn off MySQL 'strict' mode
-	echo "Turning off MySQL strict mode..."
-	sudo touch /etc/mysql/conf.d/disable_strict_mode.cnf
-	echo "[mysqld]" | sudo tee -a /etc/mysql/conf.d/disable_strict_mode.cnf > /dev/null 2>&1
-	echo "sql_mode=IGNORE_SPACE,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION" | sudo tee -a /etc/mysql/conf.d/disable_strict_mode.cnf > /dev/null 2>&1
-	sudo service mysql restart
-
 	# Install Adminer (site defined in Homestead.yaml default)
 	echo "Installing Adminer..."
 	sudo mkdir /usr/share/adminer
-	wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/index.php > /dev/null 2>&1
+	sudo wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/index.php > /dev/null 2>&1
 	
 	# Remeber this the next time we try to run this
 	echo "Headmaster VM extras have been installed."
